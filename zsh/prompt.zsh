8bfa3efb71605f530371526b5681f13b4897a7f3
Andreas <ameistad@gmail.com>
2025-01-03 21:03:17 +0100

    -

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,20 +2,18 @@ autoload -Uz add-zsh-hook
 autoload colors && colors
 setopt PROMPT_SUBST
 
-
 git=$(command -v git || echo "/usr/bin/git")
 
-# Define colors for readability
 declare -A colors=(
   [reset]="%{\e[0m%}"
   [green]="%{\e[38;2;146;156;105m%}"   # Olive green (#929c69)
-[red]="%{\e[38;2;175;91;86m%}"         # Soft red (#af5b56)
+  [red]="%{\e[38;2;175;91;86m%}"       # Soft red (#af5b56)
   [yellow]="%{\e[38;2;228;194;131m%}"  # Warm yellow (#e4c283)
   [cyan]="%{\e[38;2;0;255;255m%}"      # Cyan
   [dark-blue]="%{\e[38;2;31;82;141m%}" # Dark blue (#1f528d)
 )
 
-git_info() {
+function git_info() {
   local branch=$($git symbolic-ref --short HEAD 2>/dev/null)
   [[ -z $branch ]] && return
   local git_status=$($git status --porcelain 2>/dev/null)
@@ -24,23 +22,27 @@ git_info() {
   echo "on \e[1m${color}${branch}${colors[reset]}"
 }
 
-need_push() {
+function need_push() {
   if [[ -n $($git cherry -v @{upstream} 2>/dev/null) ]]; then
     echo " with %{\e[1m%}${colors[red]}unpushed${colors[reset]} "
   fi
 }
 
-directory_name() {
+function directory_name() {
   echo "\e[1m${colors[yellow]}%3/%\/${colors[reset]}"
 }
 
-user_and_host() {
+function user_and_host() {
   echo "\e[1m${colors[dark-blue]}%n@%m${colors[reset]}"
 }
 
-set_prompt() {
-    export PROMPT=$'$(print -P "\n$(user_and_host) -> $(directory_name) $(git_info)$(need_push)\n› ")'
-    export RPROMPT=""
+function set_prompt() {
+  export PROMPT=$'$(print -P "\n$(user_and_host) -> $(directory_name) $(git_info)$(need_push)\n› ")'
+  export RPROMPT=""
+}
+
+function set_beam_cursor() {
+  print -n '\e[5 q'
 }
 
 function title() {
@@ -51,9 +53,11 @@ function title() {
   esac
 }
 
-function set_title_and_prompt() {
-  echo -ne "\e]1;${PWD##*/}\a"
+function set_all() {
+  print -n "\e]1;${PWD##*/}\a"
   title "zsh" "%m" "%55<...<%~"
   set_prompt
+  set_beam_cursor
 }
-add-zsh-hook precmd set_title_and_prompt
+
+add-zsh-hook precmd set_all
