03c4db27ba93abdf63278dad9d522c1e7e9375d9
Andreas <ameistad@gmail.com>
2025-02-06 15:17:49 +0100

    ansible

--- modules/ansible/path.zsh
+++ modules/ansible/path.zsh
@@ -1,2 +1,5 @@
+# If ansible fails with "ERROR! A worker was found in a dead state"
+#export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+
 export ANSIBLE_INVENTORY=$HOME/.config/ansible/inventory.ini
 export ANSIBLE_CONFIG=$HOME/.config/ansible/ansible.cfg
