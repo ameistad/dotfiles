3df0f879c80f37653bfc24bdfcf5a6963731a18d
Andreas <ameistad@gmail.com>
2025-09-01 13:56:34 +0200

    update

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -2,3 +2,5 @@ vim.keymap.set('n', '<leader>sv', function()
   vim.cmd('source $MYVIMRC')
   vim.notify('✅ Neovim config reloaded!', vim.log.levels.INFO)
 end, { desc = 'Reload config' })
+
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,5 +2,20 @@ return {
   'nvim-treesitter/nvim-treesitter',
   branch = 'master',
   lazy = false,
-  build = ':TSUpdate'
+  build = ':TSUpdate',
+  config = function()
+    require("nvim-treesitter.configs").setup {
+      auto_install = true,
+      ensure_installed = { "go", "lua", "vim", "bash" },
+
+      -- Enable syntax highlighting
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+
+      -- (Optional) Enable indentation
+      indent = { enable = true },
+    }
+  end,
 }
