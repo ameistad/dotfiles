0112fb419a3730fed0b0bf7b0c1c37053a9061dc
Andreas <ameistad@gmail.com>
2025-09-06 20:27:57 +0200

    add ignore fzf

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,17 +1,21 @@
 return {
-  'ibhagwan/fzf-lua',
-  -- optional for icon support
+  "ibhagwan/fzf-lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   lazy = false,
-  -- or if using mini.icons/mini.nvim
-  -- dependencies = { 'nvim-mini/mini.icons' },
-  opts = {},
+  opts = {
+    files = {
+      fd_opts = [[--color=never --type f --hidden --follow --exclude .git --ignore-file ]]
+        .. vim.fn.stdpath("config")
+        .. "/vim-ignore",
+    },
+  },
   keys = {
-    { '<leader>sf', '<cmd>FzfLua files<cr>',      desc = 'Find files' },
-    { '<leader>sb', '<cmd>FzfLua buffers<cr>',    desc = 'Find buffers' },
-    { '<leader>sg', '<cmd>FzfLua live_grep<cr>',  desc = 'Live grep' },
-    { '<leader>sh', '<cmd>FzfLua help_tags<cr>',  desc = 'Help tags' },
-    { '<leader>st', '<cmd>FzfLua tags<cr>',       desc = 'Find tags' },
-    { '<leader>sw', '<cmd>FzfLua grep_cword<cr>', desc = 'Grep current word' },
+    { "<leader>sf", "<cmd>FzfLua files<CR>", desc = "Search Files" },
+    { "<leader>sb", "<cmd>FzfLua buffers<CR>", desc = "Find buffers" },
+    { "<leader>sg", "<cmd>FzfLua live_grep<CR>", desc = "Live grep" },
+    { "<leader>sh", "<cmd>FzfLua help_tags<CR>", desc = "Help tags" },
+    { "<leader>st", "<cmd>FzfLua tags<CR>", desc = "Find tags" },
+    { "<leader>sw", "<cmd>FzfLua grep_cword<CR>", desc = "Grep current word" },
+    { "<leader>sc", "<cmd>FzfLua files cwd~/.config<CR>", desc = "Search Files in ~/.config" },
   },
 }
