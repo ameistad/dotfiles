89443a3adf8b6c8bb06cb6d43d01d43819de47ba
Andreas <ameistad@gmail.com>
2025-09-10 20:57:44 +0200

    add nvim variables

--- zsh/modules/neovim/vars.zsh
+++ zsh/modules/neovim/vars.zsh
@@ -1,2 +1,8 @@
 # Config directory
 export XDG_CONFIG_HOME="$HOME/.config"
+
+_EDITOR="$(command -v nvim)"
+export EDITOR="$_EDITOR"
+export VISUAL="$_EDITOR"
+export SUDO_EDITOR="$_EDITOR"
+unset _EDITOR
