445aeff4fa4668839b07b0cfd0daea6ff2bf7cf1
Andreas <ameistad@gmail.com>
2025-11-11 10:19:13 +0100

    add astro

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -269,6 +269,7 @@ return {
 			-- for you, so that they are available from within Neovim.
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
+				'astro',
 				'stylua', -- Used to format Lua code
 				'goimports',
 				'gofumpt',
