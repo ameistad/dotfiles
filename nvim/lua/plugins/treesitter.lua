19db9516835bf33b902531ca456ab04c712f5eea
Andreas <ameistad@gmail.com>
2025-11-12 10:27:41 +0100

    add mdx

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,34 +1,40 @@
 return {
 	'nvim-treesitter/nvim-treesitter',
 	build = ':TSUpdate',
-	main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+	-- main = 'nvim-treesitter.configs', -- Sets main module to use for opts
 	-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-	opts = {
-		ensure_installed = {
-			'astro',
-			'bash',
-			'diff',
-			'go',
-			'html',
-			'javascript',
-			'lua',
-			'luadoc',
-			'markdown',
-			'markdown_inline',
-			'query',
-			'typescript',
-			'vim',
-			'vimdoc',
-		},
-		-- Autoinstall languages that are not installed
-		auto_install = true,
-		highlight = {
-			enable = true,
-			-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-			--  If you are experiencing weird indenting issues, add the language to
-			--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-			additional_vim_regex_highlighting = false,
-		},
-		indent = { enable = true },
-	},
+	---@diagnostic disable-next-line: missing-fields
+	config = function()
+		require('nvim-treesitter.configs').setup({
+			auto_install = true,
+			sync_install = false,
+			ignore_install = {},
+			ensure_installed = {
+				'astro',
+				'bash',
+				'css',
+				'diff',
+				'go',
+				'html',
+				'javascript',
+				'lua',
+				'luadoc',
+				'markdown',
+				'markdown_inline',
+				'query',
+				'tsx',
+				'typescript',
+				'vim',
+				'vimdoc',
+			},
+			highlight = {
+				enable = true,
+				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+				--  If you are experiencing weird indenting issues, add the language to
+				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
+				additional_vim_regex_highlighting = { 'astro' },
+			},
+			indent = { enable = true },
+		})
+	end,
 }
