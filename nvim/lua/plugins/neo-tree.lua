ad21323dd59920fe054cd322005ddf406a62dc9a
Andreas <ameistad@gmail.com>
2026-04-19 22:13:24 +0200

    update neo tree to reveal current file

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,5 +1,28 @@
 local km = require('utils.keymap_helpers')
-vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle<CR>', km.with_desc('Toggle File [E]xplorer'))
+vim.keymap.set('n', '<leader>e', function()
+	local is_open = false
+	for _, win in ipairs(vim.api.nvim_list_wins()) do
+		local buf = vim.api.nvim_win_get_buf(win)
+		if vim.bo[buf].filetype == 'neo-tree' then
+			is_open = true
+			break
+		end
+	end
+
+	if is_open then
+		vim.cmd('Neotree close')
+		return
+	end
+
+	local bufname = vim.api.nvim_buf_get_name(0)
+	local is_real_file = vim.bo.buftype == '' and bufname ~= '' and vim.fn.filereadable(bufname) == 1
+
+	if is_real_file then
+		vim.cmd('Neotree reveal')
+	else
+		vim.cmd('Neotree toggle')
+	end
+end, km.with_desc('Toggle File [E]xplorer'))
 
 return {
 	{
